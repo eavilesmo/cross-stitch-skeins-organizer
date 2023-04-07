560e63d09932676c9c2b78c4a403837787bbb3e7
Esperanza <esperanza.aviles@clarity.ai>
2023-04-07 21:42:07 +0200

    feat: add new user input options in ThreadOrganizer

--- main/ThreadOrganizer.py
+++ main/ThreadOrganizer.py
@@ -1,9 +1,10 @@
 from main.ConversionListInitializer import ConversionListInitializer
-from main.DmcToAnchorConversionList import dmc_to_anchor_list
 from main.ThreadConverter import ThreadConverter
 from main.Presenter import Presenter
+from main.ThreadRepository import ThreadRepository
 
 thread_converter = ThreadConverter()
+thread_repository = ThreadRepository()
 
 conversionListInitializer = ConversionListInitializer()
 conversion_list = conversionListInitializer.initialize()
@@ -12,11 +13,22 @@ presenter = Presenter()
 user_input = presenter.print_welcome()
 
 if user_input == "1":
-    thread_number = presenter.ask_for_thread_number()
-    print(thread_converter.convert_dmc_to_anchor(thread_number, conversion_list))
+    skein_number = presenter.ask_for_skein_number()
+    print(thread_converter.convert_dmc_to_anchor(skein_number, conversion_list))
 
 elif user_input == "2":
-    thread_number = presenter.ask_for_thread_number()
-    print(thread_converter.convert_anchor_to_dmc(thread_number, conversion_list))
+    skein_number = presenter.ask_for_skein_number()
+    print(thread_converter.convert_anchor_to_dmc(skein_number, conversion_list))
 
+elif user_input == "3":
+    file_path = presenter.ask_for_file_path()
+    thread_converter.convert_dmc_to_anchor_in_bulk(file_path, conversion_list)
+
+elif user_input == "4":
+    skein_number = presenter.ask_for_skein_number()
+    thread_repository.find_thread(skein_number)
+
+elif user_input == "5":
+    file_path = presenter.ask_for_file_path()
+    thread_repository.find_thread_in_bulk(file_path)
 
