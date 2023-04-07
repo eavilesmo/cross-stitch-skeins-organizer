7bfb2b4e6187274bb2053d584a2389ba102afb6d
Esperanza <esperanza.aviles@clarity.ai>
2023-04-07 21:38:43 +0200

    feat: add new options to the welcome menu in the Presenter class

--- main/Presenter.py
+++ main/Presenter.py
@@ -1,11 +1,17 @@
 class Presenter:
     def print_welcome(self):
         print("Welcome to the program! What would you like to do?")
-        print("1. Convert a DMC thread to Anchor")
-        print("2. Convert an Anchor thread to DMC")
-        print("3. Know how many skeins I have for this color")
+        print("1. Convert a DMC skein to Anchor")
+        print("2. Convert an Anchor skein to DMC")
+        print("3. Convert many DMC skeins to Anchor")
+        print("3. Check if I have this skein available (only one skein)")
+        print("4. Check if I have these skeins available (many skeins)")
         return input()
 
-    def ask_for_thread_number(self):
+    def ask_for_skein_number(self):
         print("Please enter the number of the skein you want to convert:")
+        return input()
+
+    def ask_for_file_path(self):
+        print("Please enter path to the file with the skeins' number")
         return input()
