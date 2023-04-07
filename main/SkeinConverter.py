dcd5a1025407ffbad01757a8cde0db712f9f45b9
Esperanza <esperanza.aviles@clarity.ai>
2023-04-07 22:14:29 +0200

    refactor: initialize conversion list in SkeinConverter class

--- main/SkeinConverter.py
+++ main/SkeinConverter.py
@@ -1,17 +1,22 @@
-class SkeinConverter:
+from main.ConversionListInitializer import ConversionListInitializer
 
-    def convert_dmc_to_anchor(self, skein_number, conversion_list):
-        for key, value in conversion_list.items():
+
+class SkeinConverter:
+    conversionListInitializer = ConversionListInitializer()
+    conversion_list = conversionListInitializer.initialize()
+
+    def convert_dmc_to_anchor(self, skein_number):
+        for key, value in self.conversion_list.items():
             if value == skein_number:
                 return key
 
-    def convert_anchor_to_dmc(self, skein_number, conversion_list):
-        return conversion_list[skein_number]
+    def convert_anchor_to_dmc(self, skein_number):
+        return self.conversion_list[skein_number]
 
-    def convert_dmc_to_anchor_in_bulk(self, file_path, conversion_list):
+    def convert_dmc_to_anchor_in_bulk(self, file_path):
         with open(file_path, 'r') as skeins_file:
             for line in skeins_file:
                 line = line.rstrip()
-                self.convert_dmc_to_anchor(line, conversion_list)
+                self.convert_dmc_to_anchor(line)
                 if not line:
                     continue
--- main/ThreadOrganizer.py
+++ main/ThreadOrganizer.py
@@ -1,4 +1,3 @@
-from main.ConversionListInitializer import ConversionListInitializer
 from main.SkeinConverter import SkeinConverter
 from main.Presenter import Presenter
 from main.SkeinRepository import SkeinRepository
@@ -6,23 +5,20 @@ from main.SkeinRepository import SkeinRepository
 skein_converter = SkeinConverter()
 skein_repository = SkeinRepository()
 
-conversionListInitializer = ConversionListInitializer()
-conversion_list = conversionListInitializer.initialize()
-
 presenter = Presenter()
 user_input = presenter.print_welcome()
 
 if user_input == "1":
     skein_number = presenter.ask_for_skein_number()
-    print(skein_converter.convert_dmc_to_anchor(skein_number, conversion_list))
+    print(skein_converter.convert_dmc_to_anchor(skein_number))
 
 elif user_input == "2":
     skein_number = presenter.ask_for_skein_number()
-    print(skein_converter.convert_anchor_to_dmc(skein_number, conversion_list))
+    print(skein_converter.convert_anchor_to_dmc(skein_number))
 
 elif user_input == "3":
     file_path = presenter.ask_for_file_path()
-    skein_converter.convert_dmc_to_anchor_in_bulk(file_path, conversion_list)
+    skein_converter.convert_dmc_to_anchor_in_bulk(file_path)
 
 elif user_input == "4":
     skein_number = presenter.ask_for_skein_number()
