76928303ede5975c5463fef0f7779beab359f2e7
Esperanza <esperanza.aviles@clarity.ai>
2023-04-07 21:50:50 +0200

    refactor: improve naming

--- main/ThreadOrganizer.py
+++ main/ThreadOrganizer.py
@@ -3,8 +3,8 @@ from main.SkeinConverter import SkeinConverter
 from main.Presenter import Presenter
 from main.SkeinRepository import SkeinRepository
 
-thread_converter = SkeinConverter()
-thread_repository = SkeinRepository()
+skein_converter = SkeinConverter()
+skein_repository = SkeinRepository()
 
 conversionListInitializer = ConversionListInitializer()
 conversion_list = conversionListInitializer.initialize()
@@ -14,21 +14,21 @@ user_input = presenter.print_welcome()
 
 if user_input == "1":
     skein_number = presenter.ask_for_skein_number()
-    print(thread_converter.convert_dmc_to_anchor(skein_number, conversion_list))
+    print(skein_converter.convert_dmc_to_anchor(skein_number, conversion_list))
 
 elif user_input == "2":
     skein_number = presenter.ask_for_skein_number()
-    print(thread_converter.convert_anchor_to_dmc(skein_number, conversion_list))
+    print(skein_converter.convert_anchor_to_dmc(skein_number, conversion_list))
 
 elif user_input == "3":
     file_path = presenter.ask_for_file_path()
-    thread_converter.convert_dmc_to_anchor_in_bulk(file_path, conversion_list)
+    skein_converter.convert_dmc_to_anchor_in_bulk(file_path, conversion_list)
 
 elif user_input == "4":
     skein_number = presenter.ask_for_skein_number()
-    thread_repository.find_skein(skein_number)
+    skein_repository.find_skein(skein_number)
 
 elif user_input == "5":
     file_path = presenter.ask_for_file_path()
-    thread_repository.find_skeins_in_bulk(file_path)
+    skein_repository.find_skeins_in_bulk(file_path)
 
