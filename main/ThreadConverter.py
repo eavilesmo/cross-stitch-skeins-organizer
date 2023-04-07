b2f39cce213f2728f50c905cba00f7e902a21527
Esperanza <esperanza.aviles@clarity.ai>
2023-04-07 21:41:12 +0200

    feat: add new functionalities to ThreadConverter class

--- main/ThreadConverter.py
+++ main/ThreadConverter.py
@@ -1,10 +1,17 @@
 class ThreadConverter:
 
-    def convert_dmc_to_anchor(self, thread_number, conversion_list):
-        for clave, valor in conversion_list.items():
-            if valor == thread_number:
-                return clave
+    def convert_dmc_to_anchor(self, skein_number, conversion_list):
+        for key, value in conversion_list.items():
+            if value == skein_number:
+                return key
 
-    def convert_anchor_to_dmc(self, thread_number, conversion_list):
-        return conversion_list[thread_number]
+    def convert_anchor_to_dmc(self, skein_number, conversion_list):
+        return conversion_list[skein_number]
 
+    def convert_dmc_to_anchor_in_bulk(self, file_path, conversion_list):
+        with open(file_path, 'r') as skeins_file:
+            for line in skeins_file:
+                line = line.rstrip()
+                self.convert_dmc_to_anchor(line, conversion_list)
+                if not line:
+                    continue
