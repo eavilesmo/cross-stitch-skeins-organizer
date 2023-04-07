8c2d49bfded63f0138b2a469ded45dd4592081af
Esperanza <esperanza.aviles@clarity.ai>
2023-04-07 22:23:44 +0200

    refactor: improve naming

--- main/ConversionListInitializer.py
+++ main/ConversionListInitializer.py
@@ -1,10 +1,10 @@
 class ConversionListInitializer:
     def initialize(self):
-        with open('archivo.txt', 'r') as f:
-            data = {}
-            for line in f:
-                parts = line.strip().split('\t')
-                key = parts[0]
-                value = parts[1]
-                data[key] = value
-        return data
+        with open('conversion_list.txt', 'r') as conversion_file:
+            conversion_list = {}
+            for line in conversion_file:
+                skein_numbers = line.strip().split('\t')
+                anchor_skein = skein_numbers[0]
+                dmc_skein = skein_numbers[1]
+                conversion_list[anchor_skein] = dmc_skein
+        return conversion_list
